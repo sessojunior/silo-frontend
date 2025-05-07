e7153ef1a6964e23106e39942ecd3f2f523addc6
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2025-05-07 15:37:39 -0300

    Adicionado nativewind e tailwind

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router'
+import 'react-native-reanimated'
+
+import '../global.css'
+
+export default function RootLayout() {
+	return (
+		<Stack>
+			<Stack.Screen name='index' options={{ headerShown: false }} />
+		</Stack>
+	)
+}
---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const App = () => {
+	return (
+		<SafeAreaView>
+			<Text className='text-blue-600 font-bold text-3xl '>Silo</Text>
+		</SafeAreaView>
+	)
+}
+
+export default App
